daef38fd14cce9645e069eda30703a4bd52b6a0d
Milena Marlim <milenamarlim.dev@gmail.com>
2022-02-03 14:27:04 -0300

    Commit com as funcoes desafio 6

--- desafio6/index.js
+++ desafio6/index.js
@@ -1,9 +1,33 @@
-// let numero1 = 8
-// let numero2 = 7
-
-function soma(numero1, numero2){
+function soma(numero1, numero2) {
     return numero1 + numero2
 }
 
-let resultadoFinalSoma = soma(8,7);
-console.log(resultadoFinalSoma)
+let resultadoFinalSoma = soma(8, 7);
+console.log('A soma dos dois números é: ' + resultadoFinalSoma)
+
+
+function subtracao(numero1, numero2) {
+    return numero1 - numero2
+}
+
+let resultadoFinalSubtracao = subtracao(10, 1)
+console.log('A subtracão dos dois números é: ' + resultadoFinalSubtracao)
+
+
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2
+}
+
+let resultadoFinalMultiplicacao = multiplicacao(4, 2)
+
+console.log('O resultado da multiplicação entre os dois números é: ' + resultadoFinalMultiplicacao)
+
+
+function divisao(numero1, numero2) {
+    return numero1 / numero2
+}
+
+let resultadoFinalDivisao = divisao(4, 2)
+console.log('O resultado da divisão entre os dois números é: ' + resultadoFinalDivisao)
+
+
